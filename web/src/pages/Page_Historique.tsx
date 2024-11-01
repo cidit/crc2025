b0c6d399b380934aefd7da5ffda44f5d5723de2a
AlexisSav772 <66709266+AlexisSav772@users.noreply.github.com>
2024-11-01 17:22:36 -0400

    pages web

---
+++ web/src/pages/Page_Historique.tsx
@@ -0,0 +1,9 @@
+export default function Page_Historique() 
+{
+    return (
+        <div>
+            <h1>Ceci est une page de l'historique.</h1>
+        </div>
+    )
+}
+
---
+++ web/src/pages/Page_Principale.tsx
@@ -0,0 +1,41 @@
+
+export default function Page_Principale() {
+    return (
+        <html>
+
+            <head>
+                <title>
+                    ClockWork Cafe
+                </title>
+            </head>
+
+            <body>
+                <h1>
+                    ClockWork cafe's 
+                </h1>
+                <div>
+                    <p>
+                        bouton pour faire la traduction
+                    </p>
+                </div>
+                <h2>
+                    paragraphe expliquant le lore du site et de la compétition
+                </h2>
+                <h2>
+                    Lien 1: contenu en lien avec la progression de la compétition
+                    (Photos, paragraphes)(problèmes rencontrés/difficultés)
+                </h2>
+                <h2>
+                    Lien 2: présentation des candidats (photos deepfaked des candidats)
+                </h2>
+                <div>
+                    <p>
+                        section multimédia
+                    </p>
+                </div>
+            </body>
+
+        </html>
+    )
+}
+
