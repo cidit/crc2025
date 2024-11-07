6fd0f9099ef6f36087e610caa029c51f6a5e7022
Squynijos <81109473+Squynijos@users.noreply.github.com>
2024-11-07 17:17:31 -0500

    Prelim part 3, LETS GO!!!!

--- python/Prelim1/part3/part_3.py
+++ python/Prelim1/part3/part_3.py
@@ -6,6 +6,20 @@ Ceci est le fichier template pour la partie 3 du Prelim 1.
 """
 import math
 
+class Node():
+    value:int
+    left = None
+    right = None
+
+    def __init__(self, value:int):
+        self.value = value
+
+    def setLeft(self, left):
+        self.left = left
+
+    def setRight(self, right):
+        self.right = right
+
 def part_3(values, letters):
     """
     Arrange the received values in a tree and return the order of the corresponding letters
@@ -26,36 +40,71 @@ def part_3(values, letters):
 
     values.sort()
 
-    
-    answ = []
-    make_inverse_solution(answ, values)
-    answ.reverse()
+    root = make_tree(values)
+    answ = BFS(root)
+
 
     return [dick[num] for num in answ]
 
 
-def split_at_center(numbers: list[int]) -> tuple[list[int]|None, int, list[int]|None]:
-    """Find center and"""
-    if len(numbers) == 1:
-        return [None, numbers[0], None]
-    center_index = math.floor(len(numbers)/2)
-    l = numbers[0:center_index]
-    center = numbers[center_index]
-    r = numbers[center_index+1:-1]
-    return [l, center, r]
+def make_tree(values:list[int])->Node:
+    #Find central index of list
+    central_i = math.floor(len(values)/2)
+    print(values)
+    print("LE CENTRE:" + str(central_i) + " " + str(values[central_i]))
+    
+    #Create Node
+    node = Node(values[central_i])
 
+    if len(values)>1:
+        node.setLeft(make_tree(values[0:central_i]))
+        node.setRight(make_tree(values[central_i+1:len(values)]))
 
-def make_inverse_solution(answ: list[int], numbers: list[int]):
-    if len(numbers) == 0:
+    print("Node OK! Value:" + str(node.value))
+    return node
+
+def BFS(root:Node)->list[int]:
+    if root is None:
         return
-    [l, center, r] = split_at_center(numbers)
-    if r is not None and l is not None:
-        make_inverse_solution(answ, r)
-        make_inverse_solution(answ, l)
-    answ.append(center)
+    
+    queue: list[Node]=[]
+    queue.append(root)
 
-class Node:
-    a: list[int]
-    b: list[int]
-    c: int
+    anws:list[int] = []
+
+    while queue:
+        node = queue.pop(0)  # take a node from the front of the queue
+        anws.append(node.value)
+
+        # enqueue left child
+        if node.left:
+            queue.append(node.left)
+        # enqueue right child
+        if node.right:
+            queue.append(node.right)
+
+    return anws
+
+
+# def split_at_center(numbers: list[int]) -> tuple[list[int]|None, int, list[int]|None]:
+#     """Find center and"""
+#     if len(numbers) == 1:
+#         return [None, numbers[0], None]
+#     center_index = math.floor(len(numbers)/2)
+#     l = numbers[0:center_index]
+#     center = numbers[center_index]
+#     r = numbers[center_index+1:-1]
+#     return [l, center, r]
+
+
+# def make_inverse_solution(answ: list[int], numbers: list[int]):
+#     if len(numbers) == 0:
+#         return
+#     [l, center, r] = split_at_center(numbers)
+#     if r is not None and l is not None:
+#         make_inverse_solution(answ, r)
+#         make_inverse_solution(answ, l)
+#     answ.append(center)
+
+
 
