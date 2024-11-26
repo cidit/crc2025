a45d7328b3a4748aa65f08c0880dc998eddeb94c
Squynijos <81109473+Squynijos@users.noreply.github.com>
2024-11-25 19:10:22 -0500

    Prog

--- robot/firmware/src/impls/swerveModule.cpp
+++ robot/firmware/src/impls/swerveModule.cpp
@@ -125,3 +125,94 @@ double SwerveModule::getCurrentAngle(){
   double angleAct = enco/4160.0 * (2*M_PI);
   return angleAct;
 }
+
+//---------------------------------
+Vec2D SC::calculate(double targetAngle, double speedFactor){
+    Vec2D vecSpeed;
+
+    double diffAngle = getDiffAngle(getCurrentAngle(), targetAngle);
+    //Serial.println(getCurrentAngle());
+
+    //Find the absolute components of the speed vector
+    vecSpeed.set_y(getAngularComponent(fabs(diffAngle), fabs(speedFactor)));
+    vecSpeed.set_x(getTranslationComponent(fabs(diffAngle), fabs(speedFactor)));
+
+    //Apply the good sign
+    vecSpeed.set_y(diffAngle < 0   ? vecSpeed.y()  : -vecSpeed.y());
+    vecSpeed.set_x(speedFactor < 0 ? -vecSpeed.x() : vecSpeed.x());
+
+    return vecSpeed;
+}
+
+
+/**
+ * Converts degres to radians
+ */
+double degToRad(double deg){
+    return deg * M_PI / 180;
+}
+
+/**
+ * Get the difference between currentAngle and targetAngle
+ * Sign indicate the rotation direction
+ * @return Return an angle value between -90 and 90 deg
+ */
+double SC::getDiffAngle(double currentAngle, double targetAngle){
+
+  if(targetAngle > 180){
+    targetAngle =targetAngle-180;
+  }
+  if(currentAngle > 180){
+    currentAngle = currentAngle-180;
+  }
+  auto angleCW = targetAngle-currentAngle;
+  auto angleCCW = 180 + angleCW;
+
+  if (abs(angleCW) > abs(angleCCW)) {
+    return angleCCW;
+  } else {
+    return angleCW;  
+  }
+
+  // NOTES : On a un problème quand on passe de 0-180 à 180-360 sur le joystick
+
+
+
+
+  
+  // if(currentAngle > 180){
+  //   currentAngle = currentAngle -180;
+  // }
+  // if(targetAngle > 180){
+  //   targetAngle = targetAngle -180;
+  // }
+
+  // auto travelAngle = targetAngle - currentAngle;
+
+  // //Change for shortest if to big
+  // if(travelAngle < -90){
+  //   travelAngle = 180 + travelAngle;
+  // }
+
+  // return travelAngle;
+}
+
+/**
+ * Get the angular component using a ratio of the difference
+ * between current and target angle, round to 0,001
+ */
+double SC::getAngularComponent(double diffAngle, double speedFactor){
+    double component = sin(degToRad(diffAngle))*speedFactor;
+    //Serial.println("AngComp: " + String(component));
+    return component;
+}
+
+/**
+ * Get the translation component using a ratio of the difference
+ * between current and target angle, round to 0,001
+ */
+double SC::getTranslationComponent(double diffAngle, double speedFactor){
+    double component = cos(degToRad(diffAngle))*speedFactor;
+    //Serial.println("TrsComp: " + String(component));
+    return component;
+}
