f31ffd9d792b38ef90e57f345c4130091db1db39
cidit <44065673+cidit@users.noreply.github.com>
2025-01-02 21:27:13 -0500

    implemented abs enco by following old main

--- robot/firmware/include/drives/swerve_module2.hpp
+++ robot/firmware/include/drives/swerve_module2.hpp
@@ -6,11 +6,13 @@ class SwerveModule : public Lifecycle
 {
 private:
     PrecisionMotor &_pma, &_pmb;
+    pin_t _e_p;
 
 public:
-    SwerveModule(PrecisionMotor &pma, PrecisionMotor &pmb)
+    SwerveModule(PrecisionMotor &pma, PrecisionMotor &pmb, pin_t abs_enco_pin)
         : _pma(pma),
-          _pmb(pmb)
+          _pmb(pmb),
+          _e_p(abs_enco_pin)
     {
     }
 
@@ -34,6 +36,10 @@ public:
 
     double get_angle()
     {
-        return -1; // TODO: unimplemented
+        // TODO: handle the encoder externally so that the pulse length can be ajusted if need be.
+        const auto MAX_PULSE_LEN = 4160.0;
+        double pulse = pulseIn(_e_p, HIGH, MAX_PULSE_LEN*2); // TODO: if there are issues later, check the pulseIn
+        double angle = (pulse / MAX_PULSE_LEN) * (2 * M_PI);
+        return angle;
     }
 };
