3d2a92a503cc12d75b01178d209fa6974d0b7c9a
cidit <44065673+cidit@users.noreply.github.com>
2025-01-05 12:20:19 -0500

    added some doc to the timer

--- robot/firmware/include/util/timer.hpp
+++ robot/firmware/include/util/timer.hpp
@@ -7,19 +7,19 @@ class Timer
 {
 
 public:
-
     /**
      * milliseconds
      */
     timestamp _delay, _last, _now;
 
-
     /**
      * @param delay the delay in milliseconds.
      * @param now the initial time in milliseconds. give it a time bigger than the initial time to delay the first execution of the timer.
+     *
+     * this class should, optimally, always be instanciated at the top level of the main and be updated in the loop, and every subsequent functions should receive a const reference to this object so that they cannot accidentally update the object, only query it throught `is_time()`.
      */
-    Timer(timestamp delay, timestamp now = 0) 
-    : _delay(delay), _last(0), _now(now) {}
+    Timer(timestamp delay, timestamp now = 0)
+        : _delay(delay), _last(0), _now(now) {}
 
     bool is_time() const
     {
@@ -28,20 +28,23 @@ public:
 
     /**
      * should be called once every loop.
-     * 
+     *
      * ```cpp
      * auto now = millis();
      * timer.update(now);
      * ```
-     * 
+     *
      * @param now the new current time.
      */
-    void update(timestamp now) {
-        if (is_time()) {
+    void update(timestamp now)
+    {
+        if (is_time())
+        {
             // before we update the current now, we must update last if it was triggered last time.
             _last = _now;
         }
-        if (_now < now) {
+        if (_now < now)
+        {
             _now = now;
         }
     }
