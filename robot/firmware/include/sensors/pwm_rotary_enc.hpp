72ebd9a992fc5066a8e79594c3d7ba787d31bd8d
cidit <44065673+cidit@users.noreply.github.com>
2025-01-15 23:49:52 -0500

    added offset to absolute pwm enco

--- robot/firmware/include/sensors/pwm_rotary_enc.hpp
+++ robot/firmware/include/sensors/pwm_rotary_enc.hpp
@@ -4,6 +4,7 @@
 #include "util/misc.hpp"
 #include "util/timer.hpp"
 #include "sensors/sensor.hpp"
+#include "math/angles.hpp"
 
 struct PwmRotaryEncoderData {
     double rads;
@@ -12,12 +13,12 @@ struct PwmRotaryEncoderData {
 class PwmRotaryEncoder : public Sensor<PwmRotaryEncoderData>
 {
     pin_t _p;
-    double _mpl;
+    double _mpl, _offset;
 
 public:
-    PwmRotaryEncoder(pin_t pin, double max_pulse_length, const Timer &poll_timer)
+    PwmRotaryEncoder(pin_t pin, double max_pulse_length, double offset, const Timer &poll_timer)
         : Sensor({0}, poll_timer),
-          _p(pin), _mpl(max_pulse_length)
+          _p(pin), _mpl(max_pulse_length), _offset(offset)
     {
     }
 
@@ -29,8 +30,9 @@ public:
     bool sample(PwmRotaryEncoderData &out) override {
         const double pulse = pulseIn(_p, HIGH, _mpl * 2); // TODO: if there are issues later, check the pulseIn
         const double angle = (pulse / _mpl) * (2 * M_PI);
+        const double offsetted = Angle::from_rad(angle + _offset)._radians;
         out = {
-            .rads = angle
+            .rads = offsetted
         };
         return true;
     }
