68628f4473cdd9b37e838dfdde6ee6c07ce23003
cidit <44065673+cidit@users.noreply.github.com>
2025-01-16 23:47:46 -0500

    made the gobuilda encoder reversible

--- robot/firmware/include/sensors/gobuilda_rotary_enc.hpp
+++ robot/firmware/include/sensors/gobuilda_rotary_enc.hpp
@@ -19,17 +19,19 @@ public:
     Encoder &_ie;
     int32_t _ie_current, _ie_old;
     const double _tpt;
+    bool _is_reversed;
 
     GobuildaRotaryEncoder(
         Encoder &internal_encoder,
         double ticks_per_turn,
-        const Timer &polling_timer)
+        const Timer &polling_timer,
+        const bool reversed)
         : Sensor({
                      .rads = 0,
                      .rpm = 0,
                  },
                  polling_timer),
-          _ie(internal_encoder), _ie_current(0), _ie_old(0), _tpt(ticks_per_turn)
+          _ie(internal_encoder), _ie_current(0), _ie_old(0), _tpt(ticks_per_turn), _is_reversed(reversed)
     {
     }
 
@@ -46,7 +48,7 @@ public:
 
     bool sample(GobuildaRotaryEncoderData &out) override
     {
-        const auto angle = _ticks_to_angle(_ie.read());
+        const auto angle = _ticks_to_angle(_internal_read());
         const auto freq = _polling_timer._delay == 0
                               ? 0
                               : ONE_SECOND / _polling_timer._delay;
@@ -62,7 +64,7 @@ public:
     {
         // speed will only calculate correctly if we poll, which makes sense.
         _ie_old = _ie_current;
-        _ie_current = _ie.read();
+        _ie_current = _internal_read();
 
         Sensor::poll();
     }
@@ -75,5 +77,10 @@ public:
         auto wrapped = fmod(ticks, _tpt);
         auto ratio = wrapped / _tpt;
         return Angle::from_ratio(ratio);
+    }
+
+    int32_t _internal_read() {
+        auto read = _ie.read();
+        return _is_reversed? -read: read;
     }
 };
