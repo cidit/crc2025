7af06f51de27fd307912aa5b5dc065675e0f456e
cidit <44065673+cidit@users.noreply.github.com>
2025-01-18 21:05:16 -0500

    added test mains for spi comm

---
+++ robot/firmware/src/main-test_spi_alduino.cpp
@@ -0,0 +1,46 @@
+/*
+spi slave
+*/
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "communication/enco_peripherals.hpp"
+
+// double dataFrame[16] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1, 11.2, 12.3, 13.4, 14.5, 15.6, 16.7};
+// int byteIndex = 0;  // Current byte to send
+// int dataSize = sizeof(dataFrame);  // Total size of the array in bytes
+// byte* dataPtr = (byte*)dataFrame;  // Pointer to the array as bytes
+
+bool does_master_need_data()
+{
+    return digitalRead(SS) == LOW;
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(SS, INPUT);
+    SPI.begin(SS);
+
+    // SPI.setClockDivider(SPI_CLOCK_DIV8); // ajust to match clock speed with master
+}
+
+void loop()
+{
+
+    if (does_master_need_data())
+    {
+        Serial.println("answering master...");
+        dataframe_softcast df;
+        df.data = {{{1.1, 2.1}, {3.1, 4.1}, {5.1, 6.1}, {9.1, 10.1}, {11.1, 12.1}, {13.1, 14.1}, {15.1, 16.1}}};
+        SPI.beginTransaction(SPI_AL_CRC_SETTINGS);
+        for (auto rawb : df.bytes)
+        {
+            auto recv = SPI.transfer(rawb);
+            Serial.print("received: ");
+            Serial.println(recv);
+        }
+        SPI.endTransaction();
+    }
+}
---
+++ robot/firmware/src/main-test_spi_crcduino.cpp
@@ -0,0 +1,56 @@
+/*
+spi master
+*/
+
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "communication/enco_peripherals.hpp"
+#include "util/timer.hpp"
+#include "util/print.hpp"
+
+Timer spi_poll_timer(ONE_SECOND/4);
+
+void init() {
+    pinMode(SS, OUTPUT);
+    digitalWrite(SS, HIGH);
+}
+
+dataframe retrieve_df() {
+    dataframe_softcast dfs;
+
+    SPI.beginTransaction(SPI_AL_CRC_SETTINGS);
+    digitalWrite(SS, LOW); // INITIATE COMM WITH SLAVE
+    for (auto i = 0; i < sizeof(dfs.bytes); i++) {
+        dfs.bytes[i] = SPI.transfer(i);
+    }
+    digitalWrite(SS, HIGH); // END COMM
+    SPI.endTransaction();
+
+    return dfs.data;
+}
+
+void setup() {
+    Serial.begin(115200);
+    
+    init();
+    SPI.begin();
+    // SPI.begin(SS);
+    // SPI.setDataMode(SS, SPI_MODE0);
+    // SPI.setClockDivider(SS, SPI_CLOCK_DIV8); // ajust to match clock speed with master
+}
+
+void loop() {
+    spi_poll_timer.update(millis());
+
+    if (spi_poll_timer.is_time()) {
+        auto df = retrieve_df();
+        for (auto data: df.data) {
+            SPRINT("v:" + String(data.rpm));
+            SPRINT("a:"+ String(data.rads));
+            SEPARATOR;
+        }
+        SPRINT('\n');
+    }
+    
+}
