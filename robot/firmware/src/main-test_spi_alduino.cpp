e7ec9e485c793d69d9a6c5d381a18cacee94b05e
cidit <44065673+cidit@users.noreply.github.com>
2025-01-18 23:15:20 -0500

    dbg: trying something new with spi

--- robot/firmware/src/main-test_spi_alduino.cpp
+++ robot/firmware/src/main-test_spi_alduino.cpp
@@ -2,55 +2,73 @@
 // spi slave
 // */
 
-// // #include <ArduinoSTL.h>
-// #include <Arduino.h>
-// #include <SPI.h>
-// #include "communication/enco_peripherals.hpp"
+// #define youssef
 
-// // double dataFrame[16] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1, 11.2, 12.3, 13.4, 14.5, 15.6, 16.7};
-// // int byteIndex = 0;  // Current byte to send
-// // int dataSize = sizeof(dataFrame);  // Total size of the array in bytes
-// // byte* dataPtr = (byte*)dataFrame;  // Pointer to the array as bytes
+#ifndef youssef
 
-// bool does_master_need_data()
-// {
-//     return digitalRead(SS) == LOW;
-// }
+// #include <ArduinoSTL.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include "communication/enco_peripherals.hpp"
 
-// void setup()
-// {
-//     Serial.begin(115200);
+// double dataFrame[16] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1, 11.2, 12.3, 13.4, 14.5, 15.6, 16.7};
+// int byteIndex = 0;  // Current byte to send
+// int dataSize = sizeof(dataFrame);  // Total size of the array in bytes
+// byte* dataPtr = (byte*)dataFrame;  // Pointer to the array as bytes
 
-//     pinMode(SS, INPUT);
-//     SPI.begin();
+bool does_master_need_data()
+{
+    return digitalRead(SS) == LOW;
+}
 
-//     SPI.setClockDivider(SPI_CLOCK_DIV8); // ajust to match clock speed with master
-// }
+void setup()
+{
+    Serial.begin(115200);
 
-// void loop()
-// {
+    pinMode(SS, INPUT);
+    SPI.begin();
 
-//     if (does_master_need_data())
-//     {
-//         Serial.println("answering master...");
-//         dataframe_softcast df;
-//         df.data = {{{1.1, 2.1}, {3.1, 4.1}, {5.1, 6.1}, {9.1, 10.1}, {11.1, 12.1}, {13.1, 14.1}, {15.1, 16.1}}};
-//         SPI.beginTransaction(SPI_AL_CRC_SETTINGS);
-//         for (auto rawb : df.bytes)
-//         {
-//             auto recv = SPI.transfer(rawb);
-//             Serial.print(recv);
-//             Serial.print("|");
-//         }
-//         Serial.println();
-//         SPI.endTransaction();
-//     }
-// }
+    SPI.setClockDivider(SPI_CLOCK_DIV8); // ajust to match clock speed with master
+}
 
+void loop()
+{
+
+    if (does_master_need_data())
+    {
+        Serial.println("answering master...");
+        dataframe_softcast df;
+        df.data = {{{1.1, 2.1}, {3.1, 4.1}, {5.1, 6.1}, {9.1, 10.1}, {11.1, 12.1}, {13.1, 14.1}, {15.1, 16.1}}};
+
+        SPI.beginTransaction(SPI_AL_CRC_SETTINGS);
+
+        byte request = SPI.transfer(0x00);
+        if (request == 0x01)
+        {
+
+            for (auto rawb : df.bytes)
+            {
+                auto recv = SPI.transfer(rawb);
+                Serial.print(rawb);
+                Serial.print("!");
+                Serial.print(recv);
+                Serial.print("|");
+            }
+            Serial.println();
+        }
+        else
+        {
+            Serial.println("not request byte");
+        }
+
+        SPI.endTransaction();
+    }
+}
+
+#else
 
 #include <SPI.h>
 #include <Encoder.h>
-
 
 #define CS_SPI_PIN 10
 
@@ -60,67 +78,62 @@ Encoder EncoB(22, 52);
 unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
 
-
-
-typedef struct mystruct {
-  // uint32_t EncoSwerveR_A;
-  uint32_t EncoSwerveR_B;
+struct mystruct
+{
+    // uint32_t EncoSwerveR_A;
+    uint32_t EncoSwerveR_B;
 };
 
-typedef union patate {
-  mystruct spiMsg;
-  byte table[sizeof(mystruct) + 1];
+union patate
+{
+    mystruct spiMsg;
+    byte table[sizeof(mystruct) + 1];
 };
 
 patate msg;
 
-
 SPISettings ctrlSPI(1000000, MSBFIRST, SPI_MODE0);
 
-boolean runEvery(unsigned long interval) {
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    return true;
-  }
-  return false;
+void setup(void)
+{
+    Serial.begin(115200);
+    SPI.begin();
+    pinMode(10, OUTPUT);
+    digitalWrite(10, LOW);
+    // Serial.print(msg.spiMsg.JoyLeft_X);
+    // Serial.println(msg.spiMsg.JoyLeft_Y);
 }
 
+void loop(void)
+{
 
-void setup(void) {
-  Serial.begin(115200);
-  SPI.begin();
-  pinMode(10, OUTPUT);
-  digitalWrite(10, LOW);
-  // Serial.print(msg.spiMsg.JoyLeft_X);
-  // Serial.println(msg.spiMsg.JoyLeft_Y);
+    // msg.spiMsg.EncoSwerveR_A = EncoA.read();
+    // msg.spiMsg.EncoSwerveR_B = EncoB.read();
+    msg.spiMsg.EncoSwerveR_B = 260093299;
+
+    // currentMillis = millis();
+    // Serial.println("djjfks");
+
+    // if (runEvery(1000)) {
+
+    for (int i = 0; i < 4; i++)
+    {
+        // delay(200);
+        // delay(1000);
+        SPI.beginTransaction(ctrlSPI);
+        digitalWrite(10, LOW);
+        printf("index : %d ", i);
+        printf("   data : %d \n", msg.table[i]);
+        byte sentByte = msg.table[i];
+        // byte val = 15;
+        SPI.transfer(sentByte);
+        digitalWrite(10, HIGH);
+        SPI.endTransaction();
+    }
+    // Serial.println(val);
+    delay(1000);
+    // digitalWrite(CS_SPI_PIN,HIGH);
+    // }
 }
 
-void loop(void) {
-
-  // msg.spiMsg.EncoSwerveR_A = EncoA.read();
-  // msg.spiMsg.EncoSwerveR_B = EncoB.read();
-  msg.spiMsg.EncoSwerveR_B = 260093299;
-
-  // currentMillis = millis();
-  // Serial.println("djjfks");
-
-  // if (runEvery(1000)) {
-
-  for (int i = 0; i < 4; i++) {
-    //delay(200);
-    //delay(1000);
-  SPI.beginTransaction(ctrlSPI);
-  digitalWrite(10, LOW);
-  printf("index : %d ", i);
-  printf("   data : %d \n", msg.table[i]);
-  byte sentByte = msg.table[i];
-  // byte val = 15;
-  SPI.transfer(sentByte);
-  digitalWrite(10, HIGH);
-  SPI.endTransaction();
-  }
-  // Serial.println(val);
-  delay(1000);
-  // digitalWrite(CS_SPI_PIN,HIGH);
-  // }
-}
+#endif
--- robot/firmware/src/main-test_spi_crcduino.cpp
+++ robot/firmware/src/main-test_spi_crcduino.cpp
@@ -2,71 +2,80 @@
 // spi master
 // */
 
+// #define youssef
 
-// #include <Arduino.h>
-// #include <SPI.h>
-// #include "communication/enco_peripherals.hpp"
-// #include "util/constants.hpp"
-// #include "util/timer.hpp"
-// #include "util/print.hpp"
+#ifndef youssef
 
-// Timer spi_poll_timer(ONE_SECOND/1);
+#include <Arduino.h>
+#include <SPI.h>
+#include "communication/enco_peripherals.hpp"
+#include "util/constants.hpp"
+#include "util/timer.hpp"
+#include "util/print.hpp"
 
-// void myinit() {
-//     pinMode(SS, OUTPUT);
-//     digitalWrite(SS, HIGH);
-// }
+Timer spi_poll_timer(ONE_SECOND/1);
 
-// dataframe retrieve_df() {
-//     dataframe_softcast dfs;
+void myinit() {
+    pinMode(SS, OUTPUT);
+    digitalWrite(SS, HIGH);
+}
 
-//     SPI.beginTransaction(SPI_AL_CRC_SETTINGS);
-//     digitalWrite(SS, LOW); // INITIATE COMM WITH SLAVE
-//     for (uint8_t i = 0; i < sizeof(dfs.bytes); i++) {
-//         dfs.bytes[i] = SPI.transfer(i);
-//     }
-//     digitalWrite(SS, HIGH); // END COMM
-//     SPI.endTransaction();
+dataframe retrieve_df() {
+    dataframe_softcast dfs;
 
-//     return dfs.data;
-// }
+    SPI.beginTransaction(SPI_AL_CRC_SETTINGS);
+    digitalWrite(SS, LOW); // INITIATE COMM WITH SLAVE
 
-// void setup() {
-//     Serial.begin(115200);
+    SPI.transfer(0x01); // initiate request
+
+    for (uint16_t i = 0; i < sizeof(dfs.bytes); i++) {
+        dfs.bytes[i] = SPI.transfer(i);
+    }
+
+    digitalWrite(SS, HIGH); // END COMM
+    SPI.endTransaction();
+
+    return dfs.data;
+}
+
+void setup() {
+    Serial.begin(115200);
     
-//     myinit();
-//     SPI.begin();
-//     // SPI.begin(SS);
-//     // SPI.setDataMode(SS, SPI_MODE0);
-//     // SPI.setClockDivider(SS, SPI_CLOCK_DIV8); // ajust to match clock speed with master
-// }
+    myinit();
+    SPI.begin();
+    // SPI.begin(SS);
+    // SPI.setDataMode(SS, SPI_MODE0);
+    // SPI.setClockDivider(SS, SPI_CLOCK_DIV8); // ajust to match clock speed with master
+}
 
-// void loop() {
-//     spi_poll_timer.update(millis());
+void loop() {
+    spi_poll_timer.update(millis());
 
-//     if (spi_poll_timer.is_time()) {
-//         auto df = retrieve_df();
-//         for (auto data: df.data) {
-//             SPRINT("v:" + String(data.rpm));
-//             SPRINT("a:"+ String(data.rads));
-//             SEPARATOR;
-//         }
-//         SPRINT('\n');
-//     }
+    if (spi_poll_timer.is_time()) {
+        auto df = retrieve_df();
+        for (auto data: df.data) {
+            SPRINT("v:" + String(data.rpm));
+            SPRINT("a:"+ String(data.rads));
+            SEPARATOR;
+        }
+        SPRINT('\n');
+    }
     
-// }
+}
+
+#else
 
 
 
 #include <SPI.h>
 #define CS_SPI_PIN 53
 
-typedef struct mystruct {
+struct mystruct {
   // uint32_t EncoSwerveR_A;
   uint32_t EncoSwerveR_B;
 };
 
-typedef union patate {
+union patate {
   mystruct spiMsg;
   byte table[sizeof(mystruct)];
 };
@@ -106,3 +115,5 @@ void loop() {
     received = false;
   }
 }
+
+#endif
