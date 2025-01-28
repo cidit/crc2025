0ec2ffae68b5aaf86188f64226eea4cd1da1c179
Squynijos <81109473+Squynijos@users.noreply.github.com>
2025-01-28 17:23:46 -0500

    Début de la structure du programme

--- robot/firmware/include/communication/controller.hpp
+++ robot/firmware/include/communication/controller.hpp
@@ -11,23 +11,94 @@
 class Controller: public Lifecycle
 {
 public:
-    /**
-     * this struct's angle field gets updated if it's
-     * raw values' norm is 0.
-     */
-    struct joystick
-    {
-        Vec2D xy;
-        double angle;
-    } j1, j2;
+    struct Joy{
+        double x = 0;
+        double y = 0;
+        double norm = 0;
+        double angleDeg = 0;
+        double angleRad = 0;
+    };
 
-    CrcUtility::RemoteState _raw_last, _raw_current;
+    struct Buttons{
+        bool X = false;
+        bool T = false;
+        bool C = false;
+        bool R = false;
+        bool Up = false;
+        bool Down = false;
+        bool Left = false;
+        bool Right = false;
+    };
+
+    struct Gachettes{
+        int i; //Placeholder
+    };
+
+    //Joysticks
+    Joy joyLeft;
+    Joy joyRight;
+
+    //Buttons
+    Buttons buttons;
+
+    /// @brief Constructor
+    Controller() {}
     
     void begin() override {
-
+        //No-Op
     }
 
     void update() override {
-    
+        calculateJoys();
+        readButtons();
+    }
+
+    /// @brief Get and calculate values for the joysticks
+    void calculateJoys(){
+        //Get joystick position entre -1.0 et 1.0, rounded tom 0.0
+        joyLeft.x = -roundf(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK1_X)/12.80)/10.0;
+        joyLeft.y = roundf(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK1_Y)/12.80)/10.0;
+        joyRight.x = -roundf(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK2_X)/12.80)/10.0;
+        joyRight.y = roundf(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK2_Y)/12.80)/10.0;
+
+        //On enleve le dead zone au centre du joy
+        if(joyLeft.x < 0.15 && joyLeft.x > -0.15){
+            joyLeft.x = 0;
+        }
+        if(joyLeft.y < 0.15 && joyLeft.y > -0.15){
+            joyLeft.y = 0;
+        }
+        if(joyRight.x < 0.15 && joyRight.x > -0.15){
+            joyRight.x = 0;
+        }
+        if(joyRight.y < 0.15 && joyRight.y > -0.15){
+            joyRight.y = 0;
+        }
+
+        //Calcul de l'angle, ne calcule pas si le joy est au centre
+        if(joyLeft.x != 0 || joyLeft.y != 0){
+            joyLeft.angleRad = atan2(joyLeft.y, joyLeft.x) + M_PI;
+            joyLeft.angleDeg = joyLeft.angleRad * 180.0 / M_PI;
+        }
+        if(joyRight.x != 0 || joyRight.y != 0){
+            joyRight.angleRad = atan2(joyRight.y, joyRight.x) + M_PI;
+            joyRight.angleDeg = joyRight.angleRad * 180.0 / M_PI;
+        }
+
+        //Calcul de la norm 
+        joyLeft.norm = constrain(sqrt(sq(joyLeft.x)+sq(joyLeft.y)), -1.0, 1.0);
+        joyRight.norm = constrain(sqrt(sq(joyRight.x)+sq(joyRight.y)), -1.0, 1.0);
+    }
+
+    /// @brief Get values of buttons
+    void readButtons(){
+        buttons.X = CrcLib::ReadDigitalChannel(BUTTON::COLORS_DOWN);
+        buttons.T = CrcLib::ReadDigitalChannel(BUTTON::COLORS_UP);
+        buttons.C = CrcLib::ReadDigitalChannel(BUTTON::COLORS_LEFT);
+        buttons.R = CrcLib::ReadDigitalChannel(BUTTON::COLORS_RIGHT);
+        buttons.Up = CrcLib::ReadDigitalChannel(BUTTON::ARROW_UP);
+        buttons.Down = CrcLib::ReadDigitalChannel(BUTTON::ARROW_DOWN);
+        buttons.Left = CrcLib::ReadDigitalChannel(BUTTON::ARROW_LEFT);
+        buttons.Right = CrcLib::ReadDigitalChannel(BUTTON::ARROW_RIGHT);
     }
 };
--- robot/firmware/src/main-crcduino.cpp
+++ robot/firmware/src/main-crcduino.cpp
@@ -1,11 +1,95 @@
+/*
+ * Programme principal du robot 
+ * Contient la logique de fonctionnement.
+ *
+*/
 
+//----- Includes -----
 #include <CrcLib.h>
+#include "communication/controller.hpp"
+#include <drives/precision_motor.hpp>
+#include <Encoder.h>
+#include <sensors/gobuilda_rotary_enc.hpp>
+#include "sensors/paul_stroffregen_enco_sensor_wrapper.hpp"
+//--------------------
+
+//----- Defines ------
+//--------------------
+
+//----- Objects ------
+Controller ctrl;
+Timer polling_timer(ONE_SECOND / 40);
+
+//Moteur pour le bras
+    //Moteur Gauche
+Motor motorLB(CRC_PWM_7);
+Encoder encoLB(CRC_I2C_SCL, CRC_DIG_5);
+EncoderWrapper enco_wrappedLB(encoLB, polling_timer);
+GobuildaRotaryEncoder roencoLB(enco_wrappedLB, 145.1 * 2.5, polling_timer, true);
+PrecisionMotor pmotorLB(motorLB, roencoLB, 400.);
+    //Moteur Droit
+Motor motorRB(CRC_PWM_7);
+Encoder encoRB(CRC_I2C_SCL, CRC_DIG_5);
+EncoderWrapper enco_wrappedRB(encoRB, polling_timer);
+GobuildaRotaryEncoder roencoRB(enco_wrappedRB, 145.1 * 2.5, polling_timer, true);
+PrecisionMotor pmotorRB(motorRB, roencoRB, 400.);
+    //Moteur Poignet
+Motor motorPB(CRC_PWM_7);
+Encoder encoPB(CRC_I2C_SCL, CRC_DIG_5);
+EncoderWrapper enco_wrappedPB(encoPB, polling_timer);
+GobuildaRotaryEncoder roencoPB(enco_wrappedPB, 145.1 * 2.5, polling_timer, true);
+PrecisionMotor pmotorPB(motorPB, roencoPB, 400.);
+    //Moteur Servo
+    //TODO
+
+//Moteur pour swerve A
+    //Moteur A
+    //TODO
+    //Moteur B
+    //TODO
+
+//Moteur pour swerve B
+    //Moteur A
+    //TODO
+    //Moteur B
+    //TODO
+
+//--------------------
+
+//----- Variables ----
+//--------------------
 
 
+//----- Main Prog ----
 void setup(){
-
+    Serial.begin(115200);
+    CrcLib::Initialize();
 }
 
 void loop(){
+    //Updates every object
+    CrcLib::Update();
+    ctrl.update();
+    
+    //Gère les différents objects selon les inputs
+    lanceur();
+    swerve();
+    bras();
+}
+//--------------------
+
+/// @brief Logique du lanceur
+void lanceur(){
 
 }
+
+/// @brief Logique des swerves
+void swerve(){
+
+}
+
+/// @brief Logique du bras
+void bras(){
+
+}
+//--------------------
