f752984572f2134ccee2150a8ac4e2d9cf5798ed
cidit <44065673+cidit@users.noreply.github.com>
2025-02-11 19:15:33 -0500

    updated config

--- robot/firmware/include/config.hpp
+++ robot/firmware/include/config.hpp
@@ -9,55 +9,62 @@
  */
 void pmotors_config(PrecisionMotor *pmotors)
 {
-	/* PMOTOR #[0] config */ {
+	/* PMOTOR #[0] Swerve Right B config */ {
 		auto &pm = pmotors[0];
 		pm._e.set_inverted(true);
 		pm._m.set_inverted(true);
-		pm._pid_angle.setK(0.60000, 0.000001, 0.1000);
-		pm._pid_speed.setK(0.6, 0.000001, 0.10000);
+		pm._pid_angle.setK(0.60000, 0.00000, 0.08000);
+		pm._pid_speed.setK(0.60000, 0.00000, 0.07000);
 	};
-	/* PMOTOR #[1] config */ {
+	/* PMOTOR #[1] Swerve Right A config */ {
 		auto &pm = pmotors[1];
 		pm._e.set_inverted(true);
 		pm._m.set_inverted(true);
-		pm._pid_angle.setK(0.60000, 0.000001, 0.1000);
-		pm._pid_speed.setK(0.6, 0.000001, 0.1000);
+		pm._pid_angle.setK(0.60000, 0.00000, 0.08000);
+		pm._pid_speed.setK(0.60000, 0.00000, 0.07000);
 	};
-	/* PMOTOR #[2] config */ {
+	/* PMOTOR #[2] Swerve Left A config */ {
 		auto &pm = pmotors[2];
 		pm._e.set_inverted(false);
 		pm._m.set_inverted(true);
-		pm._pid_angle.setK(0.60000, 0.000001, 0.1000);
-		pm._pid_speed.setK(0.6, 0.000001, 0.1000);
+		pm._pid_angle.setK(0.60000, 0.00000, 0.08000);
+		pm._pid_speed.setK(0.60000, 0.00000, 0.07000);
 	};
-	/* PMOTOR #[3] config */ {
+	/* PMOTOR #[3] Swerve Left B config */ {
 		auto &pm = pmotors[3];
 		pm._e.set_inverted(true);
 		pm._m.set_inverted(true);
-		pm._pid_angle.setK(0.60000, 0.000001, 0.1000);
-		pm._pid_speed.setK(0.6, 0.000001, 0.100);
-	};    
-    /* PMOTOR #[4] config Bras Droit */ {
-        auto &pm = pmotors[4];
-        pm._e.set_inverted(true);
-        pm._m.set_inverted(false);
-        pm._pid_angle.setK(0.80000, 0.00001, 0.02000);
-        pm._pid_speed.setK(0.00000, 0.00000, 0.00000);
-    };
-    /* PMOTOR #[5] config Bras Gauche */ {
-        auto &pm = pmotors[5];
-        pm._e.set_inverted(false);
-        pm._m.set_inverted(true);
-        pm._pid_angle.setK(0.80000, 0.00001, 0.02000);
-        pm._pid_speed.setK(0.00000, 0.00000, 0.00000);
-    };
-    /* PMOTOR #[6] config Poignet */ {
-        auto &pm = pmotors[6];
-        pm._e.set_inverted(false);
-        pm._m.set_inverted(false);
-        pm._pid_angle.setK(0.80000, 0.00100, 0.00100);
-        pm._pid_speed.setK(0.00000, 0.00000, 0.00000);
-    };
+		pm._pid_angle.setK(0.60000, 0.00000, 0.08000);
+		pm._pid_speed.setK(0.60000, 0.00000, 0.07000);
+	};
+	/* PMOTOR #[4] Bras Right config */ {
+		auto &pm = pmotors[4];
+		pm._e.set_inverted(true);
+		pm._m.set_inverted(false);
+		pm._pid_angle.setK(0.80000, 0.00001, 0.02000);
+		pm._pid_speed.setK(0.00000, 0.00000, 0.00000);
+	};
+	/* PMOTOR #[5] Bras Left config */ {
+		auto &pm = pmotors[5];
+		pm._e.set_inverted(false);
+		pm._m.set_inverted(true);
+		pm._pid_angle.setK(0.80000, 0.00001, 0.02000);
+		pm._pid_speed.setK(0.00000, 0.00000, 0.00000);
+	};
+	/* PMOTOR #[6] Poignet config */ {
+		auto &pm = pmotors[6];
+		pm._e.set_inverted(false);
+		pm._m.set_inverted(false);
+		pm._pid_angle.setK(0.80000, 0.00100, 0.00100);
+		pm._pid_speed.setK(0.00000, 0.00000, 0.00000);
+	};
+	/* PMOTOR #[7] Lanceur config */ {
+		auto &pm = pmotors[7];
+		pm._e.set_inverted(false);
+		pm._m.set_inverted(false);
+		pm._pid_angle.setK(0.00000, 0.00000, 0.00000);
+		pm._pid_speed.setK(0.00000, 0.00000, 0.00000);
+	};
 }
 
 /**
