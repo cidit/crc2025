2539a4cddb39eaf10f685a37164a07bc7c1d773f
cidit <44065673+cidit@users.noreply.github.com>
2025-02-15 13:11:53 -0500

    finished experimental controller

--- robot/firmware/include/communication/experimental_controller.hpp
+++ robot/firmware/include/communication/experimental_controller.hpp
@@ -13,7 +13,6 @@
 class Controller : public Lifecycle
 {
 public:
-
     /**
      * this struct's angle field gets updated if it's
      * raw values' norm is 0.
@@ -30,7 +29,7 @@ public:
         color_up, color_down, color_left, color_right,
         hat_left, hat_right,
         logo, select, start,
-        left_bumper, right_bumper;   
+        left_bumper, right_bumper;
     double left_trigger, right_trigger;
     Joystick joystick_left, joystick_right;
 
@@ -41,21 +40,37 @@ public:
 
     void begin() override
     {
-        arrow_up = Bouton(_raw.arrowUp);
-        arrow_down = Bouton(_raw.arrowDown);
-        arrow_right = Bouton(_raw.arrowRight);
-        arrow_left = Bouton(_raw.arrowLeft);
-        color_up = Bouton(_raw.colorUp);
-        color_down = Bouton(_raw.colorDown);
-        color_right = Bouton(_raw.colorRight);
-        color_left = Bouton(_raw.colorLeft);
-        hat_left = Bouton(_raw.hatL);
-        hat_right = Bouton(_raw.hatR);
-        logo = Bouton(_raw.logo);
-        select = Bouton(_raw.select);
-        start = Bouton(_raw.start);
-        left_bumper = Bouton(_raw.L1);
-        right_bumper = Bouton(_raw.R1);
+
+        arrow_up = Bouton([]()
+                          { return CrcLib::ReadDigitalChannel(BUTTON::ARROW_UP); });
+        arrow_down = Bouton([]()
+                            { return CrcLib::ReadDigitalChannel(BUTTON::ARROW_DOWN); });
+        arrow_right = Bouton([]()
+                             { return CrcLib::ReadDigitalChannel(BUTTON::ARROW_RIGHT); });
+        arrow_left = Bouton([]()
+                            { return CrcLib::ReadDigitalChannel(BUTTON::ARROW_DOWN); });
+        color_up = Bouton([]()
+                          { return CrcLib::ReadDigitalChannel(BUTTON::COLORS_UP); });
+        color_down = Bouton([]()
+                            { return CrcLib::ReadDigitalChannel(BUTTON::COLORS_DOWN); });
+        color_right = Bouton([]()
+                             { return CrcLib::ReadDigitalChannel(BUTTON::COLORS_RIGHT); });
+        color_left = Bouton([]()
+                            { return CrcLib::ReadDigitalChannel(BUTTON::COLORS_LEFT); });
+        hat_left = Bouton([]()
+                          { return CrcLib::ReadDigitalChannel(BUTTON::HATL); });
+        hat_right = Bouton([]()
+                           { return CrcLib::ReadDigitalChannel(BUTTON::HATR); });
+        logo = Bouton([]()
+                      { return CrcLib::ReadDigitalChannel(BUTTON::LOGO); });
+        select = Bouton([]()
+                        { return CrcLib::ReadDigitalChannel(BUTTON::SELECT); });
+        start = Bouton([]()
+                       { return CrcLib::ReadDigitalChannel(BUTTON::START); });
+        left_bumper = Bouton([]()
+                             { return CrcLib::ReadDigitalChannel(BUTTON::L1); });
+        right_bumper = Bouton([]()
+                              { return CrcLib::ReadDigitalChannel(BUTTON::R1); });
         left_trigger = 0;
         right_trigger = 0;
         joystick_left = {.xy = {0, 0}, .angle = 0};
@@ -66,6 +81,7 @@ public:
     {
         _raw = CrcLib::_crcXbee.State();
         _patch_raw();
+
         arrow_up.refresh();
         arrow_down.refresh();
         arrow_right.refresh();
@@ -81,20 +97,21 @@ public:
         left_bumper.refresh();
         right_bumper.refresh();
 
-        left_trigger = double(_raw.gachetteG + HALF_PWM_OUTPUT) / HALF_PWM_OUTPUT;
-        right_trigger = double(_raw.gachetteD + HALF_PWM_OUTPUT) / HALF_PWM_OUTPUT;
+        left_trigger = double(_raw.gachetteG + HALF_PWM_OUTPUT) / 255;
+        right_trigger = double(_raw.gachetteD + HALF_PWM_OUTPUT) / 255;
 
         joystick_left = _update_joystick(_raw.joystick1X, _raw.joystick1Y, joystick_left);
         joystick_right = _update_joystick(_raw.joystick2X, _raw.joystick2Y, joystick_right);
     }
 
     /// apply corrections to the inputs
-    void _patch_raw() {
+    void _patch_raw()
+    {
         // fixed analog channels the same way crclib does it under the hood
         _raw.joystick1X -= 128;
-        _raw.joystick1Y -= 128;
+        _raw.joystick1Y -= 127;
         _raw.joystick2X -= 128;
-        _raw.joystick2Y -= 128;
+        _raw.joystick2Y -= 127;
         _raw.gachetteD -= 128;
         _raw.gachetteG -= 128;
 
@@ -103,17 +120,17 @@ public:
         _raw.joystick2Y = -_raw.joystick2Y;
     }
 
-    Joystick _update_joystick(double x, double y, const Joystick &old_state)
+    const static Joystick _update_joystick(double x, double y, const Joystick &old_state)
     {
         auto xy_vec = Vec2D(x / HALF_PWM_OUTPUT,
                             y / HALF_PWM_OUTPUT);
         auto norm = xy_vec.norm();
-        constexpr auto CTLR_DEADZONE = 0.15;
+        constexpr auto CTLR_DEADZONE = 0.40;
         auto in_deadzone = abs(norm) < CTLR_DEADZONE;
 
         return in_deadzone
                    // on conserve le vieil angle et on met xy à 0
-                   ? (Joystick){.xy = Vec2D(0, 0), .angle = joystick_left.angle}
+                   ? (Joystick){.xy = Vec2D(0, 0), .angle = old_state.angle}
                    : (Joystick){.xy = xy_vec, .angle = xy_vec.angle()};
     }
 };
